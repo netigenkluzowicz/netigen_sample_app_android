09269de0f837a2074b8f7f71fb80e302ac067aac
wrobel <m.wroblewski@netigen.pl>
2022-02-28 11:23:10 +0100

    Added FlavoursConst.kt files

---
+++ app/src/gms/java/pl/netigen/sampleapp/flavour/FlavoursConst.kt
@@ -0,0 +1,23 @@
+package pl.netigen.sampleapp.flavour
+
+import pl.netigen.core.config.AppConfig
+import pl.netigen.core.main.CoreMainActivity
+import pl.netigen.coreapi.payments.IPayments
+import pl.netigen.gms.ads.AdMobAds
+import pl.netigen.gms.ads.AdMobAds.Companion.TEST_BANNER_ID
+import pl.netigen.gms.ads.AdMobAds.Companion.TEST_INTERSTITIAL_ID
+import pl.netigen.gms.ads.AdMobAds.Companion.TEST_REWARDED_ID
+import pl.netigen.gms.gdpr.GDPRConsentImpl
+import pl.netigen.gms.payments.GMSPayments
+
+object FlavoursConst {
+    const val BANNER_AD_ID: String = TEST_BANNER_ID
+    const val INTERSTITIAL_AD_ID: String = TEST_INTERSTITIAL_ID
+    const val REWARDED_AD_ID: String = TEST_REWARDED_ID
+
+    fun getPaymentsImpl(coreMainActivity: CoreMainActivity, appConfig: AppConfig): IPayments =
+        GMSPayments(coreMainActivity, inDebugMode = appConfig.inDebugMode)
+
+    fun getAdsImpl(coreMainActivity: CoreMainActivity, appConfig: AppConfig) = AdMobAds(coreMainActivity, appConfig)
+    fun getGDPRConsentImpl(coreMainActivity: CoreMainActivity) = GDPRConsentImpl(coreMainActivity)
+}
