31a8e4c705d962dca929f97502b569a1e0711f1c
wrobel <m.wroblewski@netigen.pl>
2022-03-02 12:35:48 +0100

    Converter example

--- app/src/main/java/pl/netigen/sampleapp/core/database/AppDatabase.kt
+++ app/src/main/java/pl/netigen/sampleapp/core/database/AppDatabase.kt
@@ -4,13 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import pl.netigen.sampleapp.features.musiclist.data.local.dao.MusicDao
 import pl.netigen.sampleapp.features.musiclist.data.local.model.MusicCached
 
-@Database(entities = [MusicCached::class], version = 1)
+@Database(entities = [MusicCached::class], version = 1,)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun audioDao(): MusicDao
@@ -40,6 +42,5 @@ abstract class AppDatabase : RoomDatabase() {
         }
     }
 
-    suspend fun initDatabase(context: Context) {
-    }
+    suspend fun initDatabase(context: Context) = Unit
 }
--- app/src/main/java/pl/netigen/sampleapp/core/database/Converters.kt
+++ app/src/main/java/pl/netigen/sampleapp/core/database/Converters.kt
@@ -1,3 +1,17 @@
 package pl.netigen.sampleapp.core.database
 
-class Converters
+import androidx.room.TypeConverter
+import pl.netigen.extensions.fromJson
+import pl.netigen.extensions.toJson
+
+class Converters {
+    companion object {
+        @TypeConverter
+        @JvmStatic
+        fun toJson(data: List<String>): String = data.toJson()
+
+        @TypeConverter
+        @JvmStatic
+        fun fromJson(json: String): List<String> = json.fromJson()
+    }
+}
